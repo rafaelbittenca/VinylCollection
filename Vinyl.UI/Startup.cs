0104702db32416bbaf60e85c652a8d7393b004de
rafae <rafae@DESKTOP-1Q06APQ>
2017-09-06 19:52:59 -0400

    Created UI Project

---
+++ Vinyl.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vinyl.UI.Startup))]
+namespace Vinyl.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
