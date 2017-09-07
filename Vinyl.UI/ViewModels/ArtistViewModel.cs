3efc833b8b00ef77ed689f8ee0ce78bc0017200d
rafae <rafae@DESKTOP-1Q06APQ>
2017-09-07 15:06:41 -0400

    Fix dates problems

--- Vinyl.UI/ViewModels/ArtistViewModel.cs
+++ Vinyl.UI/ViewModels/ArtistViewModel.cs
@@ -22,9 +22,9 @@ namespace Vinyl.UI.ViewModels
         [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)] //
-        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
-        [DataType(DataType.Date)]
+        // Globalization
+        //[DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)] 
         public DateTime BirthDate { get; set; }
 
         [Display(Name = "About Link (E.g. Wikipedia, Artist WebPage)")]
