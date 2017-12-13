4d239d59ea9c5c4ad5936b92a8d72cf58f109042
rafaelbittenca <rafaelbittencourt.fns@gmail.com>
2017-12-13 18:38:22 -0500

    Layout

--- Vinyl.UI/Controllers/HomeController.cs
+++ Vinyl.UI/Controllers/HomeController.cs
@@ -7,36 +7,42 @@ using Vinyl.UI.ViewModels;
 namespace Vinyl.UI.Controllers
 {
 	public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            ViewBag.GoogleMapApiKey = WebConfigurationManager.AppSettings["GoogleMapKey"];
-            return View();
-        }
+	{
+		public ActionResult Index()
+		{
+			ViewBag.GoogleMapApiKey = WebConfigurationManager.AppSettings["GoogleMapKey"];
+			return View();
+		}
 
-        public ActionResult AboutMe()
-        {
-            return View("AboutMe");
-        }
 
-        [HttpPost]
-        public ActionResult Index(EmailContact obj)
-        {
-            try
-            {
-                var email = new Email();
-                var msgBody = string.Format("<p>Hi {0},</p><p>This is a copy for your message.</p><p>Message:</p><p>{1}</p>", obj.ToName, obj.EMailBody);
-                if (email.SendEmail(obj.ToEmail, obj.EmailBCC, obj.EmailCC, obj.ToName, obj.EmailSubject, msgBody))
-                {
-                    ViewBag.Status = "Email Sent Successfully.";
-                    ModelState.Clear();
-                }
-            }
-            catch (Exception ex)
-            {
-                ViewBag.Status = string.Format("Problem while sending email, Please check details. ({0})", ex.Message);
-            }
-            return View();
-        }
-    }
+		public ActionResult Test()
+		{
+			return View("Test");
+		}
+
+		public ActionResult AboutMe()
+		{
+			return View("AboutMe");
+		}
+
+		[HttpPost]
+		public ActionResult Index(EmailContact obj)
+		{
+			try
+			{
+				var email = new Email();
+				var msgBody = string.Format("<p>Hi {0},</p><p>This is a copy for your message.</p><p>Message:</p><p>{1}</p>", obj.ToName, obj.EMailBody);
+				if (email.SendEmail(obj.ToEmail, obj.EmailBCC, obj.EmailCC, obj.ToName, obj.EmailSubject, msgBody))
+				{
+					ViewBag.Status = "Email Sent Successfully.";
+					ModelState.Clear();
+				}
+			}
+			catch (Exception ex)
+			{
+				ViewBag.Status = string.Format("Problem while sending email, Please check details. ({0})", ex.Message);
+			}
+			return View();
+		}
+	}
 }
